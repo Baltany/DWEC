bcfcd62914efebbb0eb97bf70e63f2dc0c51a1ea
Baltany <bmoylop0903@g.educaand.es>
2025-04-01 14:35:21 +0200

    Haciendo el primer ejercicio

--- RelacionEjercicios4.js
+++ RelacionEjercicios4.js
@@ -1,3 +1,15 @@
 // 1) Realizar una función javascript que invierta las palabras de una cadena de texto,  
 // Ej: invertir(“Miguel Angel”) devolverá “legnA leugiM”.  
 // Nota: Array.from(), split(), spread operator, for of
+
+function invertirPalabras(str){
+    let reversedStr = str.split(" ");
+    let reversedWords = [];
+    for (let i = 0;i < reversedStr.length; i++){  
+        let reverse = [...reversedStr[i]].reverse().join("");
+        reversedWords.push(reverse);
+    }
+    return reversedWords.reverse().join(" ");
+}
+console.log(invertirPalabras("Miguel Angel"));
+
