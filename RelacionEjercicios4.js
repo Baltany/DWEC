37b2d0839b824d3857128ebb13380abe384033cd
Baltany <bmoylop0903@g.educaand.es>
2025-04-01 14:39:45 +0200

    Haciendo el primer ejercicio

--- RelacionEjercicios4.js
+++ RelacionEjercicios4.js
@@ -2,13 +2,20 @@
 // Ej: invertir(“Miguel Angel”) devolverá “legnA leugiM”.  
 // Nota: Array.from(), split(), spread operator, for of
 
+/* Función invertir cadena realizada con un fori */ 
 function invertirPalabras(str){
     let reversedStr = str.split(" ");
     let reversedWords = [];
+    /*recorremos la cadena con un fori para ir invirtiendo las palabras*/
     for (let i = 0;i < reversedStr.length; i++){  
+        // nos creamos una variable con la cual vamos a invertir la palabra,
+        // necesitamos el join para unirlo
         let reverse = [...reversedStr[i]].reverse().join("");
+        // añadimos la palabra invertida al array
         reversedWords.push(reverse);
     }
+    // devolvemos el array con las palabras invertidas, unidas por un espacio
+    //  y otra vez invertido para que  nos lo devuelva como pide el ejercicio
     return reversedWords.reverse().join(" ");
 }
 console.log(invertirPalabras("Miguel Angel"));
