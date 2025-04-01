a4a2eb309e54690f98922ddf7b8cb91e13071e23
Baltany <bmoylop0903@g.educaand.es>
2025-04-01 14:07:24 +0200

    First commit

---
+++ RelacionEjercicios4.js
@@ -0,0 +1,3 @@
+// 1) Realizar una función javascript que invierta las palabras de una cadena de texto,  
+// Ej: invertir(“Miguel Angel”) devolverá “legnA leugiM”.  
+// Nota: Array.from(), split(), spread operator, for of
