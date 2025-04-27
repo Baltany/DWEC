f7658be30ceeb9b7a2faf10b4b4c0a2b320eefb9
Balbino <balbino@balbino.balbino>
2025-04-27 20:25:24 +0200

    ejercicio 5 cifrar incompletto

---
+++ segundoejercicio/quintoApartado/ejecicio5.js
@@ -0,0 +1,11 @@
+let cifrar = document.getElementById("cifrar");
+
+cifrar.addEventListener("click", function(){
+    event.preventDefault();
+    let msg = document.getElementById("mensaje").value;
+
+});
+
+function cifrarMensaje(msg){
+    return 0;
+}
