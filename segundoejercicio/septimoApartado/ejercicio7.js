be591d165381919181d4d230f07feb9692ea1644
Balbino <balbino@balbino.balbino>
2025-05-07 14:01:07 +0200

    ejercicio 7 inicio

--- segundoejercicio/septimoApartado/ejercicio7.js
+++ segundoejercicio/septimoApartado/ejercicio7.js
@@ -1,9 +1,30 @@
-let tablero = document.getElementById("tablero");
-// vamso a definir el tamaño del tablero
+let div = document.getElementById("tablero");
+// vamos a definir el tamaño del tablero
 let filas = 10;
 let columnas = 10;
+// creamos con array que es el tablero lleno de 0 con el fill bidimensional
+const tablero = Array.from({length:filas},() => Array(columnas).fill(0));
 
 
-function pintarTablero(){
+
+function pintarTablero(tablero){
+    div.innerHTML = "";
+    const tabla = document.createElement("table");
+
+    for (let i = 0; i < filas; i++) {
+        const tr = document.createElement("tr");
+        for (let j = 0; j < columnas; j++) {
+            const td = document.createElement("td");
+            // mostramos barco
+            td.textContent = tablero[i][j] !== 0 ? "■" : "";
+            td.style.width = "30px";
+            td.style.height = "30px";
+            td.style.border = "1px solid black";
+            td.style.textAlign = "center";
+            tr.appendChild(td);
+        }
+    tabla.appendChild(tr);
+    }
+    div.appendChild(tabla);
 
 }
