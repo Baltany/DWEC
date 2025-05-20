774793bd95259812bf571840ee1b4bfea1138f06
Baltany <bmoylop0903@g.educaand.es>
2025-05-20 14:27:27 +0200

    Haciendo el examen

--- examen_balbino/ejercicio2.js
+++ examen_balbino/ejercicio2.js
@@ -2,7 +2,37 @@
 // otra idéntica pero en la que se invierta primero la primera fila con la última fila y después se invierta la primera columna
 // con la última coluna (2 puntos)
 
-const matriz = document.getElementById("matriz");
+
+const matriz = document.getElementById("matrizOriginal");
+const matrizInvertida = document.getElementById("matrizInvertida");
+const filas = 10;
+const columnas = 10;
+const tablero = [];
+for(let i = 0; i < filas; i++){
+    tablero[i] = [];
+    for(let j = 0; j < columnas; j++){
+        tablero[i][j] = 0;
+    }
+}
+
+
+// pintamos la matriz original
+for(let i = 0; i < filas; i++){
+    const fila = [];
+    for(let j = 0; j < columnas; j++){
+        fila.push(getRandomInt(10, 80));
+    }
+    tablero.push(fila);
+}
+// pintamos la matriz invertida
+for(let i = 0; i < filas; i++){
+    const fila = [];
+    for(let j = 0; j < columnas; j++){
+        fila.push(tablero[filas - 1 - i][j]);
+    }
+    tablero.push(fila);
+}
+
 
 // rellanar la matriz de numeros aleatorios entre el 10 y 80
 function generarAleatorio(){
@@ -17,12 +47,26 @@ function generarAleatorio(){
 // Comproibando que el numero aleatorio da entre 10-80
 console.log(generarAleatorio());
 
-function crearMatriz(){
-    for(let i = 0; i < 10 ;i++){
-        const fila = document.createElement("tr");
-        for(let j = 0;j < 10;j++){
-            const celda = document.createElement("td");
-            //celda.id = 
-        }
-    }
+//rellenamos la matriz con numeros aleatorios
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+// function crearMatriz(){
+//     // yo he puesto que empiece en 10 pero pdría haber cogido un numeor aletaroio
+//     let numero = 1;
+//     for(let i = 0; i < 10 ;i++){
+//         const fila = document.createElement("tr");
+//         for(let j = 0;j < 10;j++){
+//             const celda = document.createElement("td");
+//             celda.id = "celda-" + numero;
+//             fila.appendChild(celda);
+//             numero++;
+//         }
+//     matriz.appendChild(fila);
+//     }
+// }
+
