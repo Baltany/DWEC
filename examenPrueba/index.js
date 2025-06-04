2b718f2afebc8ed11048c2473a2686f03bf35ad1
Balbino <balbino@balbino.balbino>
2025-06-04 13:33:07 +0200

    Poyecto Angular

--- examenPrueba/index.js
+++ examenPrueba/index.js
@@ -28,7 +28,7 @@ document
     .addEventListener("change", function () {
         const valor = this.value;
         const contenedor = document.getElementById("contenedorMarcaModelo");
-        contenedor.innerHTML = ""; 
+        contenedor.innerHTML = "";
 
         if (valor === "portatil") {
             const marca = document.createElement("input");
@@ -42,4 +42,36 @@ document
             contenedor.appendChild(marca);
             contenedor.appendChild(modelo);
         }
-});
+    });
+
+// apartado d
+function validarFormulario() {
+    const nombre = document
+        .getElementById("nombre")
+        .value;
+    const apellidos = document
+        .getElementById("apellidos")
+        .value;
+    const telefono = document
+        .getElementById("telefono")
+        .value;
+    const serie = document
+        .getElementById("serie")
+        .value;
+    const tipo = document
+        .getElementById("tipoReparacion")
+        .value;
+    const sintomas = document.querySelectorAll('input[name="sintomas"]:checked');
+
+    if (!nombre || !apellidos || !telefono || !serie || !tipo || sintomas.length === 0) {
+        alert("Por favor, rellene todos los campos obligatorios.");
+        return false;
+    }
+    return true;
+}
+
+// apartado e
+function validarContrasena(pass) {
+    const patron = /^_[0-9]{3}[A-Z][a-z]_$/
+    return patron.test(pass);
+}
